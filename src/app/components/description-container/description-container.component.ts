76b83966914260be6ea94383fef8a289bbc505b7
Benjamin <benjamin.bats.dev@gmail.com>
2022-06-14 11:48:14 +0200

    basic description

--- src/app/components/description-container/description-container.component.ts
+++ src/app/components/description-container/description-container.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-description-container',
   templateUrl: './description-container.component.html',
-  styleUrls: ['./description-container.component.scss']
+  styleUrls: ['./description-container.component.scss'],
 })
 export class DescriptionContainerComponent implements OnInit {
+  exempleItem1 = 'exempleItem1';
+  exempleItem2 = 'exempleItem2';
+  exempleItem3 = 'exempleItem3';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
